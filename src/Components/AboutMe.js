45e52ec17decf62cf44a935086f082af602f323b
AbigailSwigert <abigail@swigertdesigns.com>
2020-09-30 13:30:40 -0500

    switched images from using import to require

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import '../App.css';
 import {Link} from 'react-router-dom';
-import logo from '../assets/swigert_designs_logo_white.png';
-import echo from '../assets/ECHO.png';
-import otherWorlds from '../assets/OTHER WORLDS THAN THESE.png';
-import trials from '../assets/TRIALS.png';
-import faultline from '../assets/FAULTLINE.png';
-import headshot from '../assets/Profile Photo.jpg';
 
 function AboutMe() {
     return (
         <div className="App">
             <div className="navPane full-bg">
-                <Link to='/'><img id='logo' src={logo} alt='Swigert Designs logo in white' /></Link>
+                <Link to='/'><img id='logo' src={require('../assets/swigert_designs_logo_white.png')} alt='Swigert Designs logo in white' /></Link>
                 <header>
                     <h1 className="titleText">About Me</h1>
                     <nav>
@@ -38,25 +32,25 @@ function AboutMe() {
                 </p>
                 <div id="music">
                     <a href="https://www.youtube.com/watch?v=KCec0v0Btbo" target="_blank" rel="noopener noreferrer" aria-roledescription="image">
-                        <img className="musicIcons" src={echo} alt="Circular icon for the Starset song Echo in black and white" />
+                        <img className="musicIcons" src={require('../assets/ECHO.png')} alt="Circular icon for the Starset song Echo in black and white" />
                     </a>
 
                     <a href="https://www.youtube.com/watch?v=wHK1q8srtMo" target="_blank" rel="noopener noreferrer" aria-roledescription="image">
-                        <img className="musicIcons" src={otherWorlds} alt="Circular icon for the Starset song Other Worlds Than These in black and white" />
+                        <img className="musicIcons" src={require('../assets/OTHER WORLDS THAN THESE.png')} alt="Circular icon for the Starset song Other Worlds Than These in black and white" />
                     </a>
 
                     <a href="https://www.youtube.com/watch?v=0dG9pXeOgT0" target="_blank" rel="noopener noreferrer" aria-roledescription="image">
-                        <img className="musicIcons" src={trials} alt="Circular icon for the Starset song Trials in black and white" />
+                        <img className="musicIcons" src={require('../assets/TRIALS.png')} alt="Circular icon for the Starset song Trials in black and white" />
                     </a>
 
                     <a href="https://www.youtube.com/watch?v=MfqVlwetWfY" target="_blank" rel="noopener noreferrer" aria-roledescription="image">
-                        <img className="musicIcons" src={faultline} alt="Circular icon for the Starset song Faultline in black and white" />
+                        <img className="musicIcons" src={require('../assets/FAULTLINE.png')} alt="Circular icon for the Starset song Faultline in black and white" />
                     </a>
                     
                 </div>
                 <small>Click these icons to hear some of my favorite STARSET songs (Opens YouTube in another window).</small>
             </div>
-            <img id="headshot" src={headshot} alt="Abigail Swigert" />
+            <img id="headshot" src={require('../assets/Profile Photo.jpg')} alt="Abigail Swigert" />
         </div>
   );
 }
