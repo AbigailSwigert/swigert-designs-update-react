7af16f21abaddb61e3aa145b722a6cd83e70ebef
Abigail Swigert <abigailswigert@Abigails-Air-2.local>
2023-03-10 10:08:19 -0600

    rearrange files, add contact menu, complete project grid

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from './Components/Header';
 import Nav from './Components/Nav';
 import ProjectGrid from './Components/ProjectGrid';
+import ContactMenu from './Components/ContactMenu';
 
 function App() {
 
   return (
     <Router>
       <Nav />
-      <Header />
-      <ProjectGrid />
+      <Routes>
+        <Route path="/"
+          exact
+          element={
+          <>
+            <Header />
+            <ProjectGrid />
+          </>}
+        />
+      </Routes>
+      <ContactMenu />
     </Router>
   );
 }
--- src/Components/ProjectGrid.jsx
+++ src/Components/ProjectGrid.jsx
@@ -1,24 +1,32 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./project-grid.css";
 
 export default function ProjectGrid() {
 	return (
-        // TODO: sort images into project folders
+		// TODO: Compress new images for web viewing
 		<main id="grid-container">
+			<Link to="/opms-ad-campaign" className="large">
+				<img
+					src={require("../portfolio_photos/OPMS/OPMS Silver Purple n Pink 2 Page.jpg")}
+					alt="OPMS Kratom 2-page magazine spread featuring floating OPMS Silver capsules and powder packaging floating on the left page and text on the right"
+					className="project"
+				/>
+			</Link>
 			<img
-				src={require("../portfolio_photos/OPMS Silver Purple n Pink 2 Page.jpg")}
-				alt="OPMS Kratom 2-page Magazine Spread"
-				className="project large"
-			/>
-            <img
-				src={require("../portfolio_photos/3D Marble Rendering.jpg")}
-				alt="3D rendering of Whole Herbs bagged powder products on marble platforms"
+				src={require("../portfolio_photos/Email Marketing/Candy-King-Vape-Out-of-This-World.gif")}
+				alt="Candy Kind vape juice floating above the moon"
 				className="project"
 			/>
-            <img
-				src={require("../portfolio_photos/Bold Idea T-Shirt Mockup Final.png")}
+			<img
+				src={require("../portfolio_photos/Bold Idea/Bold Idea T-Shirt Mockup Final.png")}
 				alt="Bold Idea t-shirts display front and back designs"
 				className="project"
+			/>
+			<img
+				src={require("../portfolio_photos/NicPack/NicPack Spread.png")}
+				alt="NicPack 2 page magazine spread featuring the NicPack product and packaging on the left and a smoking woman on the right"
+				className="project large"
 			/>
 		</main>
 	);
