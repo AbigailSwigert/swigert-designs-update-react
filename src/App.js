872142c63960ccbf1a3931a458a017fe787b99ba
Abigail Swigert <abigail@swigertdesigns.com>
2023-03-11 10:12:11 -0600

    add scroll to top functionality

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import Nav from './Components/Nav/Nav';
 import ContactMenu from './Components/Contact/ContactMenu';
 import routes from './routes';
+import ScrollToTop from './ScrollToTop';
 
 function App() {
 
   return (
     <Router>
+      <ScrollToTop />
       <Nav />
       {routes}
       <ContactMenu />
